2ff049215a3343d1f9bce47c7a0ecec831cb5b80
Kaare Rasmussen <kaare@cpan.org>
2012-02-20 16:29:23 +0000

    Import of KAARE/Role-REST-Client-0.01 from CPAN.
    gitpan-cpan-distribution: Role-REST-Client
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         KAARE/Role-REST-Client-0.01.tar.gz
    gitpan-cpan-author:       KAARE
    gitpan-cpan-maturity:     released

---
+++ lib/Role/REST/Client/Serializer.pm
@@ -0,0 +1,112 @@
+package Role::REST::Client::Serializer;
+{
+  $Role::REST::Client::Serializer::VERSION = '0.01';
+}
+use 5.010;
+use Try::Tiny;
+use Moose;
+use Moose::Util::TypeConstraints;
+
+use Data::Serializer;
+
+has 'type' => (
+    isa => enum ([qw{application/json application/xml application/yaml application/x-www-form-urlencoded}]),
+    is  => 'rw',
+	default => 'application/json',
+);
+no Moose::Util::TypeConstraints;
+has 'serializer' => (
+	isa => 'Data::Serializer',
+	is => 'ro',
+	default => \&_set_serializer,
+	lazy => 1,
+);
+
+no Moose;
+
+our %modules = (
+	'application/json' => {
+		module => 'JSON',
+	},
+	'application/xml' => {
+		module => 'XML::Simple',
+	},
+	'application/yaml' => {
+		module => 'YAML',
+	},
+	'application/x-www-form-urlencoded' => {
+		module => 'FORM',
+	},
+);
+
+sub _set_serializer {
+	my $self = shift;
+	return unless $modules{$self->type};
+
+	my $module = $modules{$self->type}{module};
+	return $module if $module eq 'FORM';
+
+	return Data::Serializer->new(
+		serializer => $module,
+	);
+}
+
+sub content_type {
+	my ($self) = @_;
+	return $self->type;
+}
+
+sub serialize {
+	my ($self, $data) = @_;
+	return unless $self->serializer;
+
+	my $result;
+	try {
+		$result = $self->serializer->raw_serialize($data)
+	} catch {
+		warn "Couldn't serialize data with " . $self->type;
+	};
+
+	return $result;
+}
+
+sub deserialize {
+	my ($self, $data) = @_;
+	return unless $self->serializer;
+
+	my $result;
+	try {
+		$result = $self->serializer->raw_deserialize($data);
+	} catch {
+		warn "Couldn't deserialize data with " . $self->type;
+	};
+
+	return $result;
+}
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Role::REST::Client::Serializer
+
+=head1 VERSION
+
+version 0.01
+
+=head1 AUTHOR
+
+Kaare Rasmussen <kaare at cpan dot net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Kaare Rasmussen.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
